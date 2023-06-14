7bd9b61796bbb0068f04b771ae12c4b63bb4a103
winkCodeGit <wink.code@gmail.com>
2023-06-14 23:22:07 +0530

    node boiler

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send({"message":"API working"});
+  res.send({"id":1,"message":"done"});
 });
 
 const port = process.env.PORT || 3000;
